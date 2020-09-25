59d1066e776b21d948b57183e292daf6b9f3e184
Dang Huynh <huynhminhdang@gmail.com>
2020-09-26 02:10:23 +0800

    Update withd.fish

--- withd.fish
+++ withd.fish
@@ -1,4 +1,8 @@
 function withd --argument-names directory --description "execute command in specified directory"
   set --local command $argv[2..-1]
-  command fish --private --command="cd '$directory'; $command;"
+  if test (realpath .) = (realpath $directory)
+    echo $command | source
+  else
+    command fish --private --command="cd '$directory'; $command;"
+  end
 end
