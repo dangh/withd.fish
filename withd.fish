40170b4f689fb63ca0dbc38b68bfd8ce71b68d21
Dang <huynhminhdang@gmail.com>
2020-12-11 16:52:31 +0800

    return status of executed command

--- withd.fish
+++ withd.fish
@@ -2,7 +2,14 @@ function withd --argument-names directory --description "execute command in spec
   set --local command $argv[2..-1]
   if test (realpath .) = (realpath $directory)
     echo $command | source
+    return $status
   else
-    command fish --private --command="cd '$directory'; $command;"
+    set --local status_file (mktemp)
+    set --local cmd_status
+    echo 1 > $status_file
+    command fish --private --command="cd '$directory'; and $command; and echo yes; and echo 0 > $status_file;"
+    read cmd_status < $status_file
+    rm $status_file
+    return $cmd_status
   end
 end
