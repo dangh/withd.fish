b0bf25a834637a18497338812fdb062739f648dc
Dang <dang@travelstop.com>
2020-12-30 03:45:23 +0800

    use bash to change dir, preserve PATH in subshell

--- withd.fish
+++ withd.fish
@@ -1,15 +1,5 @@
 function withd --argument-names directory --description "execute command in specified directory"
+  test -d $directory || return 1
   set --local command $argv[2..-1]
-  if test (realpath .) = (realpath $directory)
-    echo $command | source
-    return $status
-  else
-    set --local status_file (mktemp)
-    set --local cmd_status
-    echo 1 > $status_file
-    command fish --private --command="cd '$directory'; and $command; and echo yes; and echo 0 > $status_file;"
-    read cmd_status < $status_file
-    rm $status_file
-    return $cmd_status
-  end
+  command sh --noprofile --norc -c "cd $directory; $SHELL --private --init-command='set PATH $PATH' --command='$command'"
 end
