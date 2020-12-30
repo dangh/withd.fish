ef8701a34e292eac5bdf0c0b3125893d9c3880e0
Dang <huynhminhdang@gmail.com>
2020-12-30 20:54:38 +0800

    Update withd.fish

--- withd.fish
+++ withd.fish
@@ -1,5 +1,5 @@
 function withd --argument-names directory --description "execute command in specified directory"
-  test -d $directory || return 1
+  test -d "$directory" || return 1
   set --local command $argv[2..-1]
   command sh --noprofile --norc -c "cd $directory; $SHELL --private --init-command='set PATH $PATH' --command='$command'"
 end
