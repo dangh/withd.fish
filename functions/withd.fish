51e2010db8de8d0308417bd4d878300ac507efa0
Dang <huynhminhdang@gmail.com>
2022-09-19 10:05:47 +0700

    use short flags

--- functions/withd.fish
+++ functions/withd.fish
@@ -1,5 +1,5 @@
-function withd --argument-names directory --description "execute command in specified directory"
+function withd -a directory -d "execute command in specified directory"
   test -d "$directory" || return 1
-  set --local command (string replace --all '\\"' '\\\"' -- $argv[2..-1])
+  set -l command (string replace -a '\\"' '\\\"' -- $argv[2..-1])
   command sh --noprofile --norc -c "cd $directory; $SHELL --private --command=\"$command\""
 end
