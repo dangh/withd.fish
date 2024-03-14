d57f73a98b80420e4b24c73b224fdfbb0959d4d5
Dang <huynhminhdang@gmail.com>
2024-03-14 08:18:19 +0800

    format code with fish_indent

--- functions/withd.fish
+++ functions/withd.fish
@@ -1,5 +1,5 @@
 function withd -a directory -d "execute command in specified directory"
-  test -d "$directory" || return 1
-  set -l command (string replace -a '\\"' '\\\"' -- $argv[2..-1])
-  command sh --noprofile --norc -c "cd $directory; $SHELL --private --command=\"$command\""
+    test -d "$directory" || return 1
+    set -l command (string replace -a '\\"' '\\\"' -- $argv[2..-1])
+    command sh --noprofile --norc -c "cd $directory; $SHELL --private --command=\"$command\""
 end
